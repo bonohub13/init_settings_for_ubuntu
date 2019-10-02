fe3c2a98e239fb2063e795ed1a750eee7ae198d6
Kensuke Saito <ken.saito.0813@gmail.com>
2019-10-03 02:27:10 +0900

    Update init_settings.bash

--- init_settings.bash
+++ init_settings.bash
@@ -16,24 +16,7 @@ sudo apt install -y python3 \
 #######################################
 ########## optimize .bashrc settings ##########
 echo "########## added lines ##########" >> ~/.bashrc
-echo "# chmod -R +x /enter_filename/" >> ~/.bashrc
-echo "alias mkdir='mkdir -p'" >> ~/.bashrc
-echo "alias cls='clear'" >> ~/.bashrc
-echo "alias shutdown='systemctl poweroff -i'" >> ~/.bashrc
-echo "alias reboot='systemctl reboot -i'" >> ~/.bashrc
-echo "alias vimrc='vim ~/.vimrc'" >> ~/.bashrc
-echo "alias bashrc='vim ~/.bashrc'" >> ~/.bashrc
-echo "# alias code='code-insiders'" >> ~/.bashrc # use when you want to use vscode-insiders instead of vscode
-echo " " >> ~/.bashrc
-
-echo "# ===== python settings ======" >> ~/.bashrc
-echo "alias python='python3'" >> ~/.bashrc
-echo "alias pip='pip3'" >> ~/.bashrc
-echo "alias vpip='pip freeze'" >> ~/.bashrc
-echo "alias jupyter='jupyter-notebook'" >> ~/.bashrc
-echo " " >> ~/.bashrc
-
-echo "# ===== c/c++ settings ======" >> ~/.bashrc
+echo "source ${find bashrc-settings}/settings.bash" >> ~/.bashrc
 echo "#################################" >> ~/.bashrc
 ###############################################
 

28724f546e578e246a6d210bae529b698424b7db
Kensuke Saito <ken.saito.0813@gmail.com>
2019-10-03 02:25:18 +0900

    Add files via upload

---
+++ bashrc-settings/settings.bash
@@ -0,0 +1,20 @@
+##### alias settings ##########
+# chmod -R +x /enter_filename/
+alias mkdir='mkdir -p'
+alias cls='clear'
+alias sl="ls"
+alias shutdown='systemctl poweroff -i'
+alias reboot='systemctl reboot -i'
+alias suspend="systemctl suspend -i"
+alias vimrc='vim ~/.vimrc'
+alias bashrc='vim ~/.bashrc'
+alias bashrc-settings="vim ~/.bashrc-settings/settings.bash"
+# alias code='code-insiders'
+ 
+##### python settings #########
+alias python='python3'
+alias pip='python3 -m pip'
+alias vpip='pip freeze'
+alias jupyter='jupyter-notebook'
+ 
+##### c/c++ settings ##########
